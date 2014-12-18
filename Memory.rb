0d2017139f784d943e02db83993e0179cda8a69a
Fellipe Pimentel (insidemybrain) <fellipe.bravo@gmail.com>
2014-12-18 11:35:38 -0200

    Adicionada Memória em Decimal & Hexadecimal

--- Memory.rb
+++ Memory.rb
@@ -8,23 +8,32 @@ class Memory
 			@@memory[i] = ""
 		end
 	end
-
-	# => Create Memory
-	# Address | 32 Bits Instruction
-	for i in 0...10240
-		if(i<10)
-			@@memory.store("00000"+i.to_s,"")
-		elsif(100 > i && i >= 10)
-			@@memory.store("0000"+i.to_s,"")
-		elsif(1000 > i && i >= 100)
-			@@memory.store("000"+i.to_s,"")
-		elsif(10000 > i && i >= 1000)
-			@@memory.store("00"+i.to_s,"")
-		elsif(100000 > i && i >= 10000)
-			@@memory.store("0"+i.to_s,"")
-		else
-			@@memory.store(""+i.to_s,"")
+	# => Create Memory (Hex decimal)
+	# Address (hex) | 32 Bits Instruction
+	def hexMemory
+		for i in 0...10240
+			@@memory.store("0x"+i.to_s(16),"")
 		end
+		puts @@memory
 	end
-	puts @@memory
+	# => Create Memory (Decimal)
+	# Address (decimal) | 32 Bits Instruction
+	def decMemory
+		for i in 0...10240
+			if(i<10)
+				@@memory.store("00000"+i.to_s,"")
+			elsif(100 > i && i >= 10)
+				@@memory.store("0000"+i.to_s,"")
+			elsif(1000 > i && i >= 100)
+				@@memory.store("000"+i.to_s,"")
+			elsif(10000 > i && i >= 1000)
+				@@memory.store("00"+i.to_s,"")
+			elsif(100000 > i && i >= 10000)
+				@@memory.store("0"+i.to_s,"")
+			else
+				@@memory.store(""+i.to_s,"")
+			end
+		end
+		puts @@memory
+	end
 end
